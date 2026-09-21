99c7835e1a9cf22d9c32f455fbf0ddf502c22542
HelloDenyas <poceviciusdenisbiznis@gmail.com>
2026-09-21 14:07:23 +0300

    feat: add customer CRUD API

--- backend/OrderManagement.Api/Data/AppDbContext.cs
+++ backend/OrderManagement.Api/Data/AppDbContext.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using OrderManagement.Api.Models;
 
 namespace OrderManagement.Api.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Customer> Customers => Set<Customer>();
 }
---
+++ backend/OrderManagement.Api/Models/Customer.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagement.Api.Models;
+
+public class Customer
+{
+    public const int MaxNameLength = 200;
+    public const int MaxEmailLength = 254;
+    public const int MaxPhoneLength = 50;
+
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(MaxNameLength)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(MaxEmailLength)]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [MaxLength(MaxPhoneLength)]
+    public string? Phone { get; set; }
+}
