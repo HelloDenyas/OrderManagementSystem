ee1a3149178ec3fc3025751018063dea7c08c07f
HelloDenyas <poceviciusdenisbiznis@gmail.com>
2026-09-21 13:22:40 +0300

    feat: configure PostgreSQL database infrastructure

--- backend/OrderManagement.Api/Program.cs
+++ backend/OrderManagement.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' was not found.")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
